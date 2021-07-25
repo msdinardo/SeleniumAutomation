b365406c2d5bd12e1ff29b79c92f67ad28f79364
Manuel Di Nardo <msdinardo@gmail.com>
2021-07-25 13:10:43 -0500

    Add auto-screenshots to failed tests the images are going to be saved in "src/test/resources/screenshots/"

--- src/test/java/taudemo/BaseTest.java
+++ src/test/java/taudemo/BaseTest.java
@@ -2,16 +2,18 @@ package taudemo;
 
 import Util.ConfigReader;
 import Util.DriverFactory;
-import org.openqa.selenium.By;
+import com.google.common.io.Files;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import pages.HomePage;
 
+import java.io.File;
 import java.net.MalformedURLException;
-import java.util.List;
 
 public class BaseTest {
 
@@ -28,9 +30,24 @@ public class BaseTest {
         homePage = new HomePage(driver);
     }
 
+    @AfterMethod
+    public void recordFailure(ITestResult result) {
+        if (result.getStatus() == ITestResult.FAILURE) {
+            try {
+                TakesScreenshot camera = (TakesScreenshot) driver;
+                File screenshot = camera.getScreenshotAs(OutputType.FILE);
+                File dest = new File("src/test/resources/screenshots/" + result.getName() + ".png");
+                System.out.println("The screenshot has been saved in: " + screenshot.getAbsolutePath());
+                System.out.println("The screenshot is going to be moved to: " + dest.getAbsolutePath());
+                Files.move(screenshot, dest);
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+
     @AfterClass
-    public void tearDown()
-    {
+    public void tearDown() {
         driver.quit();
     }
 }
